944d4cc9e203adfa50cb2a7ddfe87cd900ea3cfe
Netson Cavina <netsoncavina@gmail.com>
2023-03-22 20:01:20 -0300

    Primeiro commit

--- App.js
+++ App.js
@@ -1,11 +1,97 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  Pressable,
+} from "react-native";
+import * as Animatable from "react-native-animatable";
 
+import images from "./utils";
 export default function App() {
+  const [pokemon, setPokemon] = useState("");
+  const [evolutions, setEvolutions] = useState([]);
+  const initials = [0, 3, 6, 9, 12, 15, 18, 21, 24];
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text style={styles.titulo}>Selecione seu inicial!</Text>
+      <View style={{ height: 150 }}>
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={true}>
+          <View style={styles.imageContainer}>
+            {initials.map((initial) => (
+              <Pressable
+                onPress={() => {
+                  setPokemon(images[initial].name);
+                  setEvolutions([initial + 1, initial + 2]);
+                }}
+                key={Math.random()}
+              >
+                <View
+                  style={{
+                    flexDirection: "column",
+                    marginLeft: 20,
+                  }}
+                >
+                  <Image
+                    key={Math.random()}
+                    style={styles.image}
+                    source={images[initial].image}
+                  />
+                  <Text key={Math.random()} style={styles.titulo}>
+                    {images[initial].name}
+                  </Text>
+                </View>
+              </Pressable>
+            ))}
+          </View>
+        </ScrollView>
+      </View>
+      {pokemon ? (
+        <View style={{ marginTop: 100 }} key={Math.random()}>
+          <Text style={styles.titulo}>Você escolheu o {pokemon}</Text>
+
+          <View style={styles.imageContainer}>
+            {evolutions.map((evolution) => (
+              <View
+                style={{
+                  flexDirection: "column",
+                  marginLeft: 20,
+                }}
+                key={Math.random()}
+              >
+                <Image
+                  key={Math.random()}
+                  style={styles.image}
+                  source={images[evolution].image}
+                />
+                <Text key={Math.random()} style={styles.titulo}>
+                  {images[evolution].name}
+                </Text>
+              </View>
+            ))}
+          </View>
+        </View>
+      ) : (
+        <View style={{ marginTop: 100 }}>
+          <Text style={styles.titulo}>Selecione um inicial</Text>
+          <Animatable.Image
+            animation="rotate"
+            duration={1000}
+            source={require("./assets/pokeball.png")}
+            style={{
+              width: 200,
+              height: 200,
+              marginBottom: 20,
+              marginRight: 20,
+            }}
+            iterationCount={"infinite"}
+          />
+        </View>
+      )}
     </View>
   );
 }
@@ -13,8 +99,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    marginTop: 80,
+  },
+  titulo: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  imageContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  image: {
+    width: 100,
+    height: 110,
+    // marginLeft: 20,
+    // marginRight: 20,
   },
 });
---
+++ utils.js
@@ -0,0 +1,139 @@
+const images = [
+  {
+    id: 1,
+    image: require("./assets/001.png"),
+    name: "Bulbasauro",
+  },
+  {
+    id: 2,
+    image: require("./assets/002.png"),
+    name: "Ivysaur",
+  },
+  {
+    id: 3,
+    image: require("./assets/003.png"),
+    name: "Venusaur",
+  },
+  {
+    id: 4,
+    image: require("./assets/004.png"),
+    name: "Charmander",
+  },
+  {
+    id: 5,
+    image: require("./assets/005.png"),
+    name: "Charmeleon",
+  },
+  {
+    id: 6,
+    image: require("./assets/006.png"),
+    name: "Charizard",
+  },
+  {
+    id: 7,
+    image: require("./assets/007.png"),
+    name: "Squirtle",
+  },
+  {
+    id: 8,
+    image: require("./assets/008.png"),
+    name: "Wartortle",
+  },
+  {
+    id: 9,
+    image: require("./assets/009.png"),
+    name: "Blastoise",
+  },
+  {
+    id: 152,
+    image: require("./assets/152.png"),
+    name: "Chikorita",
+  },
+  {
+    id: 153,
+    image: require("./assets/153.png"),
+    name: "Bayleef",
+  },
+  {
+    id: 154,
+    image: require("./assets/154.png"),
+    name: "Meganium",
+  },
+  {
+    id: 155,
+    image: require("./assets/155.png"),
+    name: "Cyndaquil",
+  },
+  {
+    id: 156,
+    image: require("./assets/156.png"),
+    name: "Quilava",
+  },
+  {
+    id: 157,
+    image: require("./assets/157.png"),
+    name: "Typhlosion",
+  },
+  {
+    id: 158,
+    image: require("./assets/158.png"),
+    name: "Totodile",
+  },
+  {
+    id: 159,
+    image: require("./assets/159.png"),
+    name: "Croconaw",
+  },
+  {
+    id: 160,
+    image: require("./assets/160.png"),
+    name: "Feraligatr",
+  },
+  {
+    id: 252,
+    image: require("./assets/252.png"),
+    name: "Treecko",
+  },
+  {
+    id: 253,
+    image: require("./assets/253.png"),
+    name: "Grovyle",
+  },
+  {
+    id: 254,
+    image: require("./assets/254.png"),
+    name: "Sceptile",
+  },
+  {
+    id: 255,
+    image: require("./assets/255.png"),
+    name: "Torchic",
+  },
+  {
+    id: 256,
+    image: require("./assets/256.png"),
+    name: "Combusken",
+  },
+  {
+    id: 257,
+    image: require("./assets/257.png"),
+    name: "Blaziken",
+  },
+  {
+    id: 258,
+    image: require("./assets/258.png"),
+    name: "Mudkip",
+  },
+  {
+    id: 259,
+    image: require("./assets/259.png"),
+    name: "Marshtomp",
+  },
+  {
+    id: 260,
+    image: require("./assets/260.png"),
+    name: "Swampert",
+  },
+];
+
+export default images;
